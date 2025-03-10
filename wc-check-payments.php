4831902350d58063d4644c12d5b186432c8e9435
rayflores <rayflores88@gmail.com>
2025-03-10 00:37:42 -0400

    bump version

--- wc-check-payments.php
+++ wc-check-payments.php
@@ -3,7 +3,7 @@
  * Plugin Name: WC Check Payments
  * Plugin URI: https://rayflores.com
  * Description: A simple plugin to add check payments to WooCommerce.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Ray Flores
  * Author URI: https://rayflores.com
  * License: GPL2
